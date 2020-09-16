dc3a800f00c7cfa048f1e8378b32437040591069
Arslan Ameen <arslan.virk11@gmail.com>
2020-09-16 13:18:31 +0500

    Update index.js

--- src/index.js
+++ src/index.js
@@ -3,7 +3,9 @@ import ReactDOM from 'react-dom';
 import App from './App';
 
 ReactDOM.render(
-   <div>Index.js main Root entry js file that Render all others js files<App name = "Arslan" qualification = "Software Engineer"/></div>,
+  <div>Index.js main Root entry js file that Render all others js files
+     <App name="Arslan" qualification="Software Engineer" />
+  </div>,
   document.getElementById('root')
 );
 
